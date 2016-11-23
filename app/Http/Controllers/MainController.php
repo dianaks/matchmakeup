ea3f99c9db95677b381cd0820be903e3992796d8
adhikasp <adhika.setyap@gmail.com>
2016-11-23 14:56:52 +0700

    Tidy up files

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,30 +7,25 @@ use League\ColorExtractor\ColorExtractor;
 use League\ColorExtractor\Palette;
 
 /*
-	Controller berfungsi untuk mengarahkan ke halaman html yang dituju 
-	dan mempersiapkan semua yang harus disiapkan sebelum halaman html dibuka
+Controller berfungsi untuk mengarahkan ke halaman html yang dituju
+dan mempersiapkan semua yang harus disiapkan sebelum halaman html dibuka
 */
 
 class MainController extends Controller
 {
     public function home()
     {
-    	$alamatFile = resource_path('assets/img/skin.jpg');
-    	$palette = Palette::fromFilename($alamatFile); // Mengambil pallete warna dari skin.jpg
+        $alamatFile = resource_path('assets/img/skin.jpg');
+        $palette = Palette::fromFilename($alamatFile); // Mengambil pallete warna dari skin.jpg
 
-    	$extractor = new ColorExtractor($palette);
-		$topFive = $extractor->extract(5);
+        $extractor = new ColorExtractor($palette);
+        $topFive = $extractor->extract(5);
 
-		foreach ($topFive as &$warna) {
-			$warna = dechex($warna);			
-		}
+        foreach ($topFive as &$warna) {
+            $warna = dechex($warna);
+        }
 
-		//Menampilkan variabel dalam PHP ke HTML
-    	$nama = 'diana';
-    	$hasil = 95 * 17;
-
-    	$maybellinePallete = ['E4BE97','E0B48F','E2AA77','E6B280','E5B177','D19B6C'];
-        return view('home', ['nama'=> $nama, 'hasil'=> $hasil, 'abc'=> $topFive]); // Menampilkan halaman home
-
+        $maybellinePallete = ['E4BE97','E0B48F','E2AA77','E6B280','E5B177','D19B6C'];
+        return view('home', ['abc'=> $topFive]); // Menampilkan halaman home
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,62 +1,58 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>MatchMakeup</title>
-		<!--bootsrtap library link-->
-		<link href="{{ asset('library/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css') }}" rel="stylesheet" media="screen">
-		<link href="{{ asset('library/bootstrap-3.3.7-dist/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
-		<link href="{{ asset('library/bootstrap-3.3.7-dist/js/bootstrap.min.js') }}" rel="stylesheet" media="screen">
-		<!--additional-->
-		<link href="{{ asset('library/bootstrap-3.3.7-dist/css/heroic-features.css') }}" rel="stylesheet" media="screen">
-	</head>
-	<body>
+<head>
+  <title>MatchMakeup</title>
+  <!--bootsrtap library link-->
+  <link href="{{ asset('library/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css') }}" rel="stylesheet" media="screen">
+  <link href="{{ asset('library/bootstrap-3.3.7-dist/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
+  <link href="{{ asset('library/bootstrap-3.3.7-dist/js/bootstrap.min.js') }}" rel="stylesheet" media="screen">
+  <!--additional-->
+  <link href="{{ asset('library/bootstrap-3.3.7-dist/css/heroic-features.css') }}" rel="stylesheet" media="screen">
+</head>
+<body>
+  <div class="container">
+    <!-- Jumbotron Header -->
+    <header class="jumbotron hero-spacer">
+      <h1>MatchMakeup</h1>
+      <p>MatchMakeup adalah sistem pendukung keputusan yang dibuat untuk menentukan warna makeup yang sesuai dengan warna alami yang dimiliki agar mengurangi potensi kesalahan pengguna dalam membeli produk makeup.</p>
 
+      <p>Kenali warna kulit anda!</p>
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" class="btn btn-primary btn-large" value="Upload Image" name="submit">
+      </form>
+    </header>
 
+    <hr>
 
-		<div class="container">
+    <!-- Title -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h3>Kulit anda terdiri dari warna-warna berikut:</h3>
+      </div>
+    </div>
+    <!-- /.row -->
 
-        <!-- Jumbotron Header -->
-        <header class="jumbotron hero-spacer">
-            <h1>MatchMakeup</h1>
-            <p>MatchMakeup adalah sistem pendukung keputusan yang dibuat untuk menentukan warna makeup yang sesuai dengan warna alami yang dimiliki agar mengurangi potensi kesalahan pengguna dalam membeli produk makeup.</p>
+    <!-- Page Features -->
 
-            <p>Kenali warna kulit anda!</p>
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-		    Select image to upload:
-		    <input type="file" name="fileToUpload" id="fileToUpload">
-		    <input type="submit" class="btn btn-primary btn-large" value="Upload Image" name="submit">
-		</form>
-        </header>
-
-        <hr>
-
-        <!-- Title -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Kulit anda terdiri dari warna-warna berikut:</h3>
-            </div>
+    <div class="row text-center">
+      @foreach($abc as $warna)
+      <div class="col-md-3 col-sm-6 hero-feature">
+        <div class="thumbnail">
+          <div style="width: 250px; height: 150px; background-color: #{{$warna}}">
+          </div>
+          <div class="caption">
+            <h3>#{{$warna}}</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p>
+              <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+            </p>
+          </div>
         </div>
-        <!-- /.row -->
+      </div>
+      @endforeach
 
-        <!-- Page Features -->
-        
-        <div class="row text-center">
-        	@foreach($abc as $warna)
-			<div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <div style="width: 250px; height: 150px; background-color: #{{$warna}}">
-					</div>
-                    <div class="caption">
-                        <h3>#{{$warna}}</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-			@endforeach
-
-        </div>
-	</body>
-</html>
+    </div>
+  </body>
+  </html>
